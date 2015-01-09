57c8ee458ae0578e1804430c1968ecfef4e7a183
Nicholas Morey - Home <theyoungermorey@gmail.com>
2015-01-09 14:53:53 -0500

    Fixed re-upvoting posts
    Now checks file for previous post ids, if the file isn't found, it
    creates a new one and logs all the post ids that it up votes.

--- up_vote_bot.py
+++ up_vote_bot.py
@@ -9,14 +9,25 @@ def voting_main(user_name):
 		print('Input was null, reverting to default! (tecnoman)')
 		user_name = 'tecnoman'
 
+	post_id = []
+
+	### Open post_id_'user_name' file and read in already voted ids
+	try:
+		with open('post_id_'+user_name, 'r') as post_id_file:
+			print('Previously up voted IDs found')
+			for line in post_id_file.read().splitlines():
+				post_id.append(line)
+	except FileNotFoundError as no_file_err:
+		print('No previously up voted IDs found')
+
 	### Connect to Reddit
 	print('Connecting...')
-	r = praw.Reddit(user_agent='The bot to hoard it all')
-	r.login('datahoardingbot', 'fuckYouC0g3c0')
+	r = praw.Reddit(user_agent='The bot to hoard it all') # Bot information
+	r.login('datahoardingbot', 'fuckYouC0g3c0') # bot login information
 
 	try:
 		reddit_user = r.get_redditor(user_name) # Get reddit user information
-	except Exception as http_err:
+	except Exception as http_err: # If the username returns 404
 		print(http_err)
 		exit(0)
 
@@ -24,11 +35,17 @@ def voting_main(user_name):
 	print('Pulling Information...')
 	user_submits = reddit_user.get_submitted() # Connection to Reddit and hold information returned
 
+	print('Checking posts...')
 	for thing in user_submits:
-		submission = r.get_submission(submission_id=thing.id)
+		if not any(thing.id in ids for ids in post_id): # Check to see if the id was previously up voted
+			## Interact with Submission
+			submission = r.get_submission(submission_id=thing.id)
+			print('Up Voting...')
+			submission.upvote()
+			with open('post_id_'+user_name, 'a') as post_id_file: # Append to the bottom of the file the new post id
+				post_id_file.write(thing.id+'\n')
+		else:
+			print('Post Already Up Voted, ID: ', thing.id)
 
-		## Interact with Submission
-		print('Up Voting...')
-		submission.upvote()
 
-voting_main(str(input('Username to upvote: ')))
+voting_main(str(input('Username to up vote: ')))
