7e00a4bd245ca848d8b954d48111cdb6ad13d0fa
Tecnoman5000 <theyoungermorey@gmail.com>
2015-07-07 16:50:23 -0400

    Added headless option and command line options

--- Subreddit Interaction/wallpaper_scraper/main.py
+++ Subreddit Interaction/wallpaper_scraper/main.py
@@ -4,16 +4,16 @@ import praw
 from time import sleep, time, localtime
 import datetime
 from os import name, system
-from sys import stdin
+from sys import argv
 import urllib3
 import imghdr
 import warnings
-import getpass
 # warnings.filterwarnings("ignore")  # Ignore all warnings
 warnings.simplefilter('ignore', ResourceWarning)  # Ignore resource warnings
+headless = False
 
 
-def main(bot_pass):
+def main():
     img_dl = 0
     post_id = []
 
@@ -21,33 +21,31 @@ def main(bot_pass):
 
     try:
         with open('post_id_archive', 'r') as post_id_file:
-            print('Previously up voted IDs found')
+            if not headless:
+                print('Previously up voted IDs found')
             for line in post_id_file.read().splitlines():
                 post_id.append(line)
         post_id_file.close()
     except FileNotFoundError:
-        print('No previously archived IDs found')
+        if not headless:
+            print('No previously archived IDs found')
 
     ''' Connect to Reddit '''
-    print('Connecting...')
+    if not headless:
+        print('Connecting...')
     r = praw.Reddit(user_agent="The scripts to hoard it all")  # Bot information
-    # while True:
-    try:
-        r.login('datahoardingbot', bot_pass)  # bot login information
-    # break
-    except Exception as login_err:
-        print(login_err)
-    # continue
 
     try:
         subreddit = r.get_subreddit('wallpapers')  # Get subreddit information
     except Exception as http_err:  # If the subreddit returns 404
-        print(http_err)
+        if not headless:
+            print(http_err)
         exit(0)
     grab_limit = 200
     subreddit_posts = subreddit.get_new(limit=grab_limit)  # Pull post information
 
-    print('Checking for new wallpaper posts...')
+    if not headless:
+        print('Checking for new wallpaper posts...')
     post_num = 0
     http = urllib3.PoolManager()
     for submission in subreddit_posts:
@@ -55,7 +53,8 @@ def main(bot_pass):
         if not any(submission.id in ids for ids in post_id):  # Check to see if the id was previously up voted
             # Interact with Submission
             single_submission = r.get_submission(submission_id=submission.id)
-            print(post_num + 1, '/', grab_limit, ' :', single_submission.title, sep='')
+            if not headless:
+                print(post_num + 1, '/', grab_limit, ' :', single_submission.title, sep='')
             # print(single_submission.url)
             url = link_format(single_submission.url)  # Grab submission link url
             if url == 'bad':
@@ -63,18 +62,21 @@ def main(bot_pass):
                     post_id_file.write(submission.id + '\n')
                 with open('bad_links', 'a') as bad_links_file:  # Append to the bottom of the file the new post id
                     bad_links_file.write(single_submission.title + ': ' + single_submission.url + '\n')
-                print('')
+                if not headless:
+                    print('')
                 post_num += 1
                 continue
 
             imgur_id = get_id(url)
             url = 'https://imgur.com/download/' + imgur_id
-            print('URL: ', url)
+            if not headless:
+                print('URL: ', url)
             image = http.request('GET', url)
             img_ext = imghdr.what(imgur_id, image.data)
             if img_ext is None:
                 img_ext = 'jpg'
-            print('Ext: ', img_ext)
+            if not headless:
+                print('Ext: ', img_ext)
 
             image_name = name_format(single_submission.title, imgur_id, img_ext)
             with open('/home/tecno/Scripts/reddit-bot/Subreddit Interaction/wallpaper_scraper/saved/' + image_name,
@@ -88,9 +90,11 @@ def main(bot_pass):
             with open('post_id_archive', 'a') as post_id_file:  # Append to the bottom of the file the new post id
                 post_id_file.write(submission.id + '\n')
 
-            print('')
+            if not headless:
+                print('')
         else:
-            print(post_num + 1, '/', grab_limit, ': Post already downloaded! \r', end='', sep='')
+            if not headless:
+                print(post_num + 1, '/', grab_limit, ': Post already downloaded! \r', end='', sep='')
             sleep(0.1)
         post_num += 1
     return img_dl
@@ -132,7 +136,8 @@ def name_format(title, imgur_id, extension):
         img_name = img_title + ' ' + res + '.' + extension
     else:
         img_name = img_title + '.' + extension
-    print('Name: ', img_name)
+    if not headless:
+        print('Name: ', img_name)
     return img_name
 
 
@@ -162,96 +167,136 @@ def link_format(url):
         url_pass = True
         # print(url)
     elif url.find('/gallery/') != -1:
-        print('Gallery Link Found, id: ', url[25:])
+        if not headless:
+            print('Gallery Link Found, id: ', url[25:])
         url_pass = False
     else:  # if url does not fit any formatting rules
-        print('non-standard url Found: ', url)
+        if not headless:
+            print('non-standard url Found: ', url)
         url_pass = False
     if len(url) == 30 and url_pass:
         return url
     else:
-        print('Bad Link: ', url)
+        if not headless:
+            print('Bad Link: ', url)
         return 'bad'
 
 
 def get_id(url):
     url = url[:len(url) - 4]
     imgur_id = url[len(url) - 7:]
-    print('ID: ', imgur_id)
+    if not headless:
+        print('ID: ', imgur_id)
     if len(imgur_id) != 7:
         return
     else:
         return imgur_id
 
 
-def __init__():
+def config_wait_time(time_sec):
+    while True:
+        if time_sec == '':
+            time_sec = 60
+            break
+        try:
+            time_sec = int(time_sec)
+            if time_sec > 60:
+                time_sec /= 60
+                time_unit = 'minute/s'
+            else:
+                time_unit = 'seconds'
+            break
+        except ValueError:
+            print('Invalid Input!')
+            time_sec = str(input('Wait time? (Default 60 seconds): '))
+    return time_sec, time_unit
+
+
+def run(recursive, wait_time, time_unit):
     num_img_dl = 0
-    input_password = getpass.getpass(prompt='Data Hoarding Bot Password: ')
+    times_run = 0
+    start_time = time()
 
-    if str(input('Do you want recursive mode? (y/n)')) == 'y':
-        wait_time = str(input('Wait time? (Default 60 seconds): '))
-        if wait_time == '':
-            wait_time = 60
-        else:
-            while True:
-                if wait_time == '':
-                    wait_time = 60
-                    break
-                try:
-                    wait_time = int(wait_time)
-                    if wait_time > 60:
-                        wait_time /= 60
-                        time_unit = 'minute/s'
-                    else:
-                        time_unit = 'seconds'
-                    break
-                except ValueError:
-                    print('Invalid Input!')
-                    wait_time = str(input('Wait time? (Default 60 seconds): '))
-        times_run = 0
-        start_time = time()
+    if recursive:
         while True:
-            system('cls' if name == 'nt' else 'clear')  # Clear screen before use
-            run_time = time() - start_time
-            if run_time > 60:
-                run_time /= 60
-                run_time_unit = 'minute/s'
-            else:
-                run_time_unit = 'seconds'
+                system('cls' if name == 'nt' else 'clear')  # Clear screen before use
+                run_time = time() - start_time
+                if run_time > 60:
+                    run_time /= 60
+                    run_time_unit = 'minute/s'
+                else:
+                    run_time_unit = 'seconds'
 
-            num_img_dl += main(input_password)
-            times_run += 1
+                num_img_dl += main()
+                times_run += 1
 
-            sleep(5)
-            system('cls' if name == 'nt' else 'clear')  # Clear screen before use
+                sleep(5)
+                system('cls' if name == 'nt' else 'clear')  # Clear screen before use
 
-            current_time = str(localtime()[3]) + ':' + str(localtime()[4]) + ':' + str(localtime()[5]) + ' - ' + str(
-                localtime()[2]) + '/' + str(localtime()[1]) + '/' + str(localtime()[0])
+                current_time = str(localtime()[3]) + ':' + str(localtime()[4]) + ':' + str(localtime()[5]) + ' - ' + str(
+                    localtime()[2]) + '/' + str(localtime()[1]) + '/' + str(localtime()[0])
 
-            log_msg = current_time + ':: Total Images DL: ' + str(num_img_dl) + ' -- Total Times run: ' + str(
-                times_run) + ' -- Recursive mode: ON'
+                log_msg = current_time + ':: Total Images DL: ' + str(num_img_dl) + ' -- Total Times run: ' + str(
+                    times_run) + ' -- Recursive mode: ON'
 
-            with open('ws_log', 'a') as ws_log:  # Append to the bottom of the file the new post id
-                ws_log.write(log_msg + '\n')
+                with open('ws_log', 'a') as ws_log:  # Append to the bottom of the file the new post id
+                    ws_log.write(log_msg + '\n')
 
-            print('Times run: ', times_run, ' -- Total Run Time: ', round(run_time, 0), ' ', run_time_unit,
-                  ' -- Total # of Images Downloaded: ', num_img_dl, sep='')
-            next_run_time = datetime.datetime.now() + datetime.timedelta(minutes=round(wait_time, 2))
+                if not headless:
+                    print('Times run: ', times_run, ' -- Total Run Time: ', round(run_time, 0), ' ', run_time_unit,
+                          ' -- Total # of Images Downloaded: ', num_img_dl, sep='')
+                next_run_time = datetime.datetime.now() + datetime.timedelta(minutes=round(wait_time, 2))
 
-            print('Waiting ', round(wait_time, 2), time_unit, '[', next_run_time, '] till next check...')
+                if not headless:
+                    print('Waiting ', round(wait_time, 2), time_unit, '[', next_run_time, '] till next check...')
 
-            if time_unit == 'seconds':
-                sleep(wait_time)
-            else:
-                sleep(wait_time * 60)
+                if time_unit == 'seconds':
+                    sleep(wait_time)
+                else:
+                    sleep(wait_time * 60)
     else:
         system('cls' if name == 'nt' else 'clear')  # Clear screen before use
-        num_img_dl = main(input_password)
+        num_img_dl = main()
         current_time = str(localtime()[3]) + ':' + str(localtime()[4]) + ':' + str(localtime()[5]) + ' - ' + str(
             localtime()[2]) + '/' + str(localtime()[1]) + '/' + str(localtime()[0])
         log_msg = current_time + ':: Total Images DL: ' + str(num_img_dl) + ' -- Recursive mode: OFF'
         with open('ws_log', 'a') as ws_log:  # Append to the bottom of the file the new post id
             ws_log.write(log_msg + '\n')
+
+
+def __init__():
+    # Check command line arguments
+    wait_time = 0
+    time_unit = 'none'
+
+    if len(argv) > 1:
+        if argv[1] == "1":
+            global headless
+            headless = True
+        if len(argv) > 3:
+            if int(argv[2]) == 1:
+                recursive = True
+            wait_time = int(argv[3])
+            time_config_array = config_wait_time(wait_time)
+            wait_time = time_config_array[0]
+            time_unit = time_config_array[1]
+        else:
+            recursive = False
+        run(recursive, wait_time, time_unit)
+    else:
+        if str(input('Do you want recursive mode? (y/n)')) == 'y':
+            recursive = True
+            wait_time = str(input('Wait time? (Default 60 seconds): '))
+            if wait_time == '':
+                wait_time = 60
+            else:
+                time_config_array = config_wait_time(wait_time)
+                wait_time = time_config_array[0]
+                time_unit = time_config_array[1]
+        else:
+            recursive = False
+        run(recursive, wait_time, time_unit)
+
 
 
 if __name__ == '__main__':
